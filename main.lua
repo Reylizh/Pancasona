340bb89a9b94888861a63e35be74b559a40c094a
Reylizh <arey.co.id@gmail.com>
2025-11-29 10:57:34 +0700

    Modify script loading and add comments
    Updated script URL and added comments.

--- main.lua
+++ main.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Reylizh/Pancasona/refs/heads/main/fly", true))()
+--Thanks For Using This Script
+--Here You Go
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reylizh/Pancasona/refs/heads/main/uvuvwevwevweonyetenyevweugwemubwemossas", true))()
